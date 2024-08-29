9cb9d49dd69aac7a4c54221b2fe565acb3794e9e
GiorgosCut <giorgos.arsenal@gmail.com>
2024-08-29 14:53:15 +0300

    Fully functional app and completed the README file

--- README.md
+++ README.md
@@ -2,4 +2,21 @@
 
 A small JavaScript application which generates random nonsensical outputs about Zodiac signs
 
-Find out how your Zodiac sign is doing :0
+♍♌♋♊♈♓♐♏♑♉♒♎⛎
+
+**Find out how your Zodiac sign is doing** 🤩
+
+## How to use
+
+-> Download the app.js file
+
+-> Save it to a location in your computer
+
+-> Run the command prompt (Windows) or console (Mac)
+
+-> Type `node app.js`
+
+## Requirments
+
+In order to open run this program your computer will need to have installed the Node.js
+runtime environment, available here [Node.js](https://nodejs.org/en).
--- app.js
+++ app.js
@@ -5,13 +5,22 @@
 //STEPS TO IMPLEMENT
 
 // 1) Define arrays of potential sentence parts
-const zodiacs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
-    "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
-const actions = ["meet", "fall in love with", "give money to", "take money from",
-    "fight with", "become friends with", "get betraid by"];
-const target = ["a friend", "your best friend", "your partner", "your parents",
-    "your boss", "Elon Musk", "Pewdiepie", "a stranger"];
+const zodiacs = ["Aries'", "Taurus'", "Geminis", "Cancers", "Leos", "Virgos",
+    "Libras", "Scorpios", "Sagittarius'", "Capricorns", "Aquarius'", "Pisces'"];
+const actions = ["meet", "fall in love with", "have an argument", "share a cup of coffee",
+    "fight with", "become friends with", "betray"];
+const target = ["friend", "best friend", "beatiful person", "old lady",
+    "rich person", "Elon Musk", "Pewdiepie", "stranger"];
  
 // 2) Create randomiser function, which picks an array element for each sentence part
+const getRandomIndex = (length) => Math.floor(Math.random()*length)
 
-// 3) Display complete sentence
+// 3) Create sentence construction function and call it
+const printRandomNonsense = () => {
+    console.log("Since I know you are a fan of Zodiac signs let me tell you the future ;)");
+    console.log("I sense that " + zodiacs[getRandomIndex(zodiacs.length)] + " will " + 
+    actions[getRandomIndex(actions.length)] + " with a " + target[getRandomIndex(target.length)]
+    + ", believe it or not!!");
+}
+
+printRandomNonsense();
