b5935b9f5601f847d0477bec58103dd6fa25a586
GiorgosCut <giorgos.arsenal@gmail.com>
2024-08-29 14:10:25 +0300

    Initial Idea

---
+++ app.js
@@ -0,0 +1,11 @@
+// -> We want to generate a random nonsensical sentence
+// -> Each sentence contains 3 parts
+// -> The zodiac sign, an action, and a person (lover, friend, foe, etc. you name it)
+
+//STEPS TO IMPLEMENT
+
+// 1) Define arrays of potential sentence parts
+
+// 2) Create randomiser function, which picks an array element for each sentence part
+
+// 3) Display complete sentence
