ce6db40621f4f6438fcaba4f6bf10b71a8743151
GiorgosCut <giorgos.arsenal@gmail.com>
2024-08-29 14:31:17 +0300

    Added the arrays of data

--- app.js
+++ app.js
@@ -5,7 +5,13 @@
 //STEPS TO IMPLEMENT
 
 // 1) Define arrays of potential sentence parts
-
+const zodiacs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
+    "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const actions = ["meet", "fall in love with", "give money to", "take money from",
+    "fight with", "become friends with", "get betraid by"];
+const target = ["a friend", "your best friend", "your partner", "your parents",
+    "your boss", "Elon Musk", "Pewdiepie", "a stranger"];
+ 
 // 2) Create randomiser function, which picks an array element for each sentence part
 
 // 3) Display complete sentence
